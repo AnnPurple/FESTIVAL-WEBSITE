7d5540034e734d9b0e94990368f0185dbe4d2296
AnnPurple <112295788+AnnPurple@users.noreply.github.com>
2023-02-14 12:43:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,237 @@
+ 	var home_screen = document.getElementById("right-side");
+ 	var first_fest = document.getElementById("sinulog");
+ 	var second_fest = document.getElementById("atihan");
+ 	var third_fest = document.getElementById("dinagyang");
+ 	var fourth_fest = document.getElementById("panagbenga");
+ 	var fifth_fest = document.getElementById("moriones");
+ 	var six_fest = document.getElementById("pahiyas");
+ 	var seven_fest = document.getElementById("pintados");
+ 	var eight_fest = document.getElementById("sirong");
+ 	var nine_fest = document.getElementById("masskara");
+ 	var ten_fest = document.getElementById("giant");
+
+
+document.getElementById("one").addEventListener("click",function()
+{
+	if(first_fest.style.display == "none")
+	{
+		home_screen.style.display = "none";
+		first_fest.style.display = "block";
+		second_fest.style.display = "none";
+		third_fest.style.display = "none";
+		fourth_fest.style.display = "none";
+		fifth_fest.style.display = "none";
+		six_fest.style.display = "none";
+		seven_fest.style.display = "none";
+		eight_fest.style.display = "none";
+		nine_fest.style.display = "none";
+		ten_fest.style.display = "none";
+	}
+	else
+	{
+		first_fest.style.display = "none";
+		home_screen.style.display = "block";
+	}
+})
+
+document.getElementById("two").addEventListener("click",function()
+{
+	if(second_fest.style.display == "none")
+	{
+		home_screen.style.display = "none";
+		first_fest.style.display = "none";
+		second_fest.style.display = "block";
+		third_fest.style.display = "none";
+		fourth_fest.style.display = "none";
+		fifth_fest.style.display = "none";
+		six_fest.style.display = "none";
+		seven_fest.style.display = "none";
+		eight_fest.style.display = "none";
+		nine_fest.style.display = "none";
+		ten_fest.style.display = "none";
+	}
+	else
+	{
+		second_fest.style.display = "none";
+		home_screen.style.display = "block";
+	}
+})
+
+document.getElementById("three").addEventListener("click",function()
+{
+	if(third_fest.style.display == "none")
+	{
+		home_screen.style.display = "none";
+		first_fest.style.display = "none";
+		second_fest.style.display = "none";
+		third_fest.style.display = "block";
+		fourth_fest.style.display = "none";
+		fifth_fest.style.display = "none";
+		six_fest.style.display = "none";
+		seven_fest.style.display = "none";
+		eight_fest.style.display = "none";
+		nine_fest.style.display = "none";
+		ten_fest.style.display = "none";
+	}
+	else
+	{
+		third_fest.style.display = "none";	
+		home_screen.style.display = "block";
+	}
+})
+
+document.getElementById("four").addEventListener("click",function()
+{
+	if(fourth_fest.style.display == "none")
+	{
+		home_screen.style.display = "none";
+		first_fest.style.display = "none";
+		second_fest.style.display = "none";
+		third_fest.style.display = "none";
+		fourth_fest.style.display = "block";
+		fifth_fest.style.display = "none";
+		six_fest.style.display = "none";
+		seven_fest.style.display = "none";
+		eight_fest.style.display = "none";
+		nine_fest.style.display = "none";
+		ten_fest.style.display = "none";
+	}
+	else
+	{
+		fourth_fest.style.display = "none";
+		home_screen.style.display = "block";
+	}
+})
+
+document.getElementById("five").addEventListener("click",function()
+{
+	if(fifth_fest.style.display == "none")
+	{
+		home_screen.style.display = "none";
+		first_fest.style.display = "none";
+		second_fest.style.display = "none";
+		third_fest.style.display = "none";
+		fourth_fest.style.display = "none";
+		fifth_fest.style.display = "block";
+		six_fest.style.display = "none";
+		seven_fest.style.display = "none";
+		eight_fest.style.display = "none";
+		nine_fest.style.display = "none";
+		ten_fest.style.display = "none";
+	}
+	else
+	{
+		fifth_fest.style.display = "none";
+		home_screen.style.display = "block";
+	}
+})
+document.getElementById("six").addEventListener("click",function()
+{
+	if(six_fest.style.display == "none")
+	{
+		home_screen.style.display = "none";
+		first_fest.style.display = "none";
+		second_fest.style.display = "none";
+		third_fest.style.display = "none";
+		fourth_fest.style.display = "none";
+		fifth_fest.style.display = "none";
+		six_fest.style.display = "block";
+		seven_fest.style.display = "none";
+		eight_fest.style.display = "none";
+		nine_fest.style.display = "none";
+		ten_fest.style.display = "none";
+	}
+	else
+	{
+		six_fest.style.display = "none";
+		home_screen.style.display = "block";
+	}
+})
+document.getElementById("seven").addEventListener("click",function()
+{
+	if(seven_fest.style.display == "none")
+	{
+		home_screen.style.display = "none";
+		first_fest.style.display = "none";
+		second_fest.style.display = "none";
+		third_fest.style.display = "none";
+		fourth_fest.style.display = "none";
+		fifth_fest.style.display = "none";
+		six_fest.style.display = "none";
+		seven_fest.style.display = "block";
+		eight_fest.style.display = "none";
+		nine_fest.style.display = "none";
+		ten_fest.style.display = "none";
+	}
+	else
+	{
+		seven_fest.style.display = "none";
+		home_screen.style.display = "block";
+	}
+})
+document.getElementById("eight").addEventListener("click",function()
+{
+	if(eight_fest.style.display == "none")
+	{
+		home_screen.style.display = "none";
+		first_fest.style.display = "none";
+		second_fest.style.display = "none";
+		third_fest.style.display = "none";
+		fourth_fest.style.display = "none";
+		fifth_fest.style.display = "none";
+		six_fest.style.display = "none";
+		seven_fest.style.display = "none";
+		eight_fest.style.display = "block";
+		nine_fest.style.display = "none";
+		ten_fest.style.display = "none";
+	}
+	else
+	{
+		eight_fest.style.display = "none";
+		home_screen.style.display = "block";
+	}
+})
+document.getElementById("nine").addEventListener("click",function()
+{
+	if(nine_fest.style.display == "none")
+	{
+		home_screen.style.display = "none";
+		first_fest.style.display = "none";
+		second_fest.style.display = "none";
+		third_fest.style.display = "none";
+		fourth_fest.style.display = "none";
+		fifth_fest.style.display = "none";
+		six_fest.style.display = "none";
+		seven_fest.style.display = "none";
+		eight_fest.style.display = "none";
+		nine_fest.style.display = "block";
+		ten_fest.style.display = "none";
+	}
+	else
+	{
+		nine_fest.style.display = "none";
+		home_screen.style.display = "block";
+	}
+})
+document.getElementById("ten").addEventListener("click",function()
+{
+	if(ten_fest.style.display == "none")
+	{
+		home_screen.style.display = "none";
+		first_fest.style.display = "none";
+		second_fest.style.display = "none";
+		third_fest.style.display = "none";
+		fourth_fest.style.display = "none";
+		fifth_fest.style.display = "none";
+		six_fest.style.display = "none";
+		seven_fest.style.display = "none";
+		eight_fest.style.display = "none";
+		nine_fest.style.display = "none";
+		ten_fest.style.display = "block";
+	}
+	else
+	{
+		ten_fest.style.display = "none";
+		home_screen.style.display = "block";
+	}
+})
